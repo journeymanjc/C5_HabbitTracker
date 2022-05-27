89d5c049263c0a00b9d0d02d122700a8a70fce22
Jae Cho <jjaecho@gmail.com>
2022-05-27 15:04:47 -0400

    Initial Commit

---
+++ C5_HabbitTracker/C5_HabbitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  C5_HabbitTrackerApp.swift
+//  C5_HabbitTracker
+//
+//  Created by Jae Cho on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct C5_HabbitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
